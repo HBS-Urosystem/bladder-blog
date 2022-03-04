60e3cd0a14b3c294f145094337e1fef9af1f7172
vhollo <vhollo@protonmail.com>
2022-03-04 16:25:00 +0100

    dev commit

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -18,7 +18,7 @@
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   buildOptions: {
-    site: 'https://astro-sanity-minimal-starter.netlify.app/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    site: 'https://bladder-dev.netlify.app/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: true,      // Generate sitemap (set to "false" to disable)
   },
   // Comment out "renderers: []" to enable Astro's default component support.
