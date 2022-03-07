ad9f9f26ffe3ab99f59bcb3c00933da446d298d0
vhollo <vhollo@protonmail.com>
2022-03-07 15:21:23 +0100

    post.publishedAt && <Post>

--- astro/src/data/site.js
+++ astro/src/data/site.js
@@ -1,13 +1,13 @@
 export default {
-    title: `Astro Sanity Minimal Starter`,
-    description: `This is an example blog made with Astro.`,
-    url: `https://astro-sanity-minimal-starter.netlify.app`, // No trailing slash!
+    title: `The Bladder Blog`,
+    description: `Our company, Urosystem Ltd. is focused on spreading awareness of IC/BPS and encourages you to share our content with anyone who is affected by this disease. We hope you enjoy the read and find any helpful tips throughout this page!`,
+    url: `https://www.urosystem.com/blog`, // No trailing slash!
     // JSON LD
-    name: `Example`,
+    name: `The Bladder Blog`,
     // Facebook URL
-    facebookURL: `https://facebook.com`,
+    facebookURL: `https://www.facebook.com/UroDapter-101721465255769https://facebook.com`,
     // Twitter URL
-    twitterURL: `https://twitter.com/jaydanurwin`,
+    twitterURL: `https://twitter.com/UroSystem_Inc`,
     // Instagram URL
-    instagramURL: `https://instagram.com/jaydanurwin`,
+    instagramURL: `https://www.instagram.com/urodapter/`,
 }
