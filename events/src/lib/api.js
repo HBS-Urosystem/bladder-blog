a27750cd9721c44b18090c99b891f3fc835365d3
vhollo <vhollo@protonmail.com>
2022-09-07 09:37:06 +0200

    no events page

--- events/src/lib/api.js
+++
@@ -1,6 +0,0 @@
-export const allPosts = `*[_type == 'post']{categories[]->{slug, title},'author': author->{name, image, bio}, ...} | order(publishedAt desc)`;
-
-export const allCategoriesWithPosts = `*[_type == 'category']{"posts": *[_type == "post" && references(^._id)]{'author': author->{name, image, bio}, ...} | order(publishedAt desc), ...}`;
-
-export const allAuthors = `*[_type == 'author']`;
-
