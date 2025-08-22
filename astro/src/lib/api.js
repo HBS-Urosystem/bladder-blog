16a4ca1d84a8354676bf7aaa3d9e31ced3f7e838
vhollo <vhollo@protonmail.com>
2025-08-22 16:07:35 +0200

    Published Authors only

--- astro/src/lib/api.js
+++ astro/src/lib/api.js
@@ -2,7 +2,7 @@ export const allPosts = `*[_type == 'post']{categories[]->{slug, title},'author'
 
 export const allCategoriesWithPosts = `*[_type == 'category']{"posts": *[_type == "post" && references(^._id)]{'author': author->{name, image, bio}, ...} | order(publishedAt desc), ...}`;
 
-export const allAuthors = `*[_type == 'author']`;
+export const allAuthors = `*[_type == 'author && publishedAt != null]']`;
 
 export const allEvents = `*[_type == 'event']{...} | order(date1 desc)`;
 
